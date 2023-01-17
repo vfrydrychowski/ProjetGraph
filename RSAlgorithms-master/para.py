5085eaf25ca35d3d22828e21a6a4426fbec451bd
vfrydrychowski <frydrychowskiv@gmail.com>
2023-01-17 23:12:13 +0100

    🧵 add execution time of threads

--- RSAlgorithms-master/para.py
+++ RSAlgorithms-master/para.py
@@ -1,17 +1,27 @@
 from mpi4py import MPI
 from configx.configx_Epinions_80 import ConfigX as Ep80
 from model.social_reg import goSocialReg
+import time
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+start = time.time()
+print("thread %s started at %s" % (rank, time.localtime(start)))
 ep80 = Ep80()
 rmses, maes = goSocialReg(ep80, rank)
 rmse_avg = comm.reduce(rmses, op=MPI.SUM, root=0)/size
 mae_avg = comm.reduce(maes, op=MPI.SUM, root=0)/size
 
+end = time.time()
+print("thread %s stoped at %s" % (rank, time.localtime(end)))
+print("exec time thread %s : %s secondes" % (rank, end-start))
+
+comm.barrier()
+end = time.time()
 if rank==0:
     print("the average of rmses is %s " % rmse_avg)
     print("the average of maes is %s " % mae_avg)
+    print("total exec time : %s secondes" % (end-start))
 
