891820f0eb82ccdf482256dc3da224f6e306e1f4
vfrydrychowski <frydrychowskiv@gmail.com>
2023-01-17 23:49:11 +0100

    :bug: better timestamp

--- RSAlgorithms-master/para.py
+++ RSAlgorithms-master/para.py
@@ -8,14 +8,14 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 start = time.time()
-print("thread %s started at %s" % (rank, time.localtime(start)))
+print("thread %s started at %s" % (rank, time.strftime("%d %b %Y %H:%M:%S", time.gmtime(start))))
 ep80 = Ep80()
 rmses, maes = goSocialReg(ep80, rank)
 rmse_avg = comm.reduce(rmses, op=MPI.SUM, root=0)/size
 mae_avg = comm.reduce(maes, op=MPI.SUM, root=0)/size
 
 end = time.time()
-print("thread %s stoped at %s" % (rank, time.localtime(end)))
+print("thread %s stoped at %s" % (rank, time.strftime("%d %b %Y %H:%M:%S", time.gmtime(end))))
 print("exec time thread %s : %s secondes" % (rank, end-start))
 
 comm.barrier()
